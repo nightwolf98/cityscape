797b836b812e006a4409477497d046e3873d092a
jack9515 <jack@rickard.plus.com>
2013-06-01 21:22:32 +0100

    All fixed

--- common/minecraft/phoenix/cityscape/core/item/CoreModItem.java
+++ common/minecraft/phoenix/cityscape/core/item/CoreModItem.java
@@ -18,8 +18,6 @@ public class CoreModItem{
 	
 	//Initializing the Items
 	public static Item roadPainter;
-	public static Item humvee;
-	public static Item spaceRifle;
     
     /**
      * Adding the items to the game
@@ -28,7 +26,5 @@ public class CoreModItem{
     public static void init()
     {
     	roadPainter= new RoadPainterItem(5001);
-    	humvee = new ItemHumvee(5002);
-    	spaceRifle = new ItemSpaceRifle(5003, 15, 12, 2001, 0, "bow.ogg", "");
     }
 }
